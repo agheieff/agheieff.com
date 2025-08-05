3be7855ecb2fc3204c031c9c74494c0521cca5b2
agheieff <agheieff@pm.me>
2025-08-05 21:48:52 +0200

    Update worker to use correct Pages preview URL

--- workers/dev-access-control.js
+++ workers/dev-access-control.js
@@ -12,7 +12,7 @@ export default {
     
     // If IP matches, fetch the actual page content
     const url = new URL(request.url);
-    url.hostname = 'agheieff-com-dev.pages.dev'; // This will be the Pages deployment URL
+    url.hostname = 'dc4f120e.agheieff-com.pages.dev'; // Pages preview deployment URL
     
     return fetch(url.toString(), request);
   }
