26a568c7d816dd053856704bcd533bcfd836f099
agheieff <agheieff@pm.me>
2025-08-11 19:31:06 +0200

    Update worker to latest deployment with Russian gallery

--- workers/dev-access-control.js
+++ workers/dev-access-control.js
@@ -12,7 +12,7 @@ export default {
     
     // If IP matches, fetch the actual page content
     const url = new URL(request.url);
-    url.hostname = '43d35f47.agheieff-com.pages.dev'; // Pages preview deployment URL
+    url.hostname = 'e551ac3a.agheieff-com.pages.dev'; // Pages preview deployment URL
     
     return fetch(url.toString(), request);
   }
