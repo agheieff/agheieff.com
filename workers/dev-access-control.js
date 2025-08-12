6d49c5f2686511b88a36b56d9bfd09b0f62dfe1b
agheieff <agheieff@pm.me>
2025-08-12 19:23:48 +0200

    Fix photo counter to stay relative to photo bottom edge during loading

--- workers/dev-access-control.js
+++ workers/dev-access-control.js
@@ -12,7 +12,7 @@ export default {
     
     // If IP matches, fetch the actual page content
     const url = new URL(request.url);
-    url.hostname = '4fdfb273.agheieff-com.pages.dev'; // Pages preview deployment URL
+    url.hostname = 'ac524b9f.agheieff-com.pages.dev'; // Pages preview deployment URL
     
     return fetch(url.toString(), request);
   }
