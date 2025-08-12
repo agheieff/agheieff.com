f17c1f567b0fad37f403c4f6eaf59793bafd9369
agheieff <agheieff@pm.me>
2025-08-12 22:16:59 +0200

    Update worker to point to bb543ee deployment

--- workers/dev-access-control.js
+++ workers/dev-access-control.js
@@ -12,7 +12,7 @@ export default {
     
     // If IP matches, fetch the actual page content
     const url = new URL(request.url);
-    url.hostname = 'ac524b9f.agheieff-com.pages.dev'; // Pages preview deployment URL
+    url.hostname = '89269215.agheieff-com.pages.dev'; // Pages preview deployment URL
     
     return fetch(url.toString(), request);
   }
